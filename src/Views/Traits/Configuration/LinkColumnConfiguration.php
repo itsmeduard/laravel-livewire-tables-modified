6c927f823498513b080c30948f5ee5a22219d964
Eduard Valdez <67836621+itsmeduard@users.noreply.github.com>
2022-04-17 10:13:53 +0800

    Update LinkColumnConfiguration.php
    getting the id for modal

--- src/Views/Traits/Configuration/LinkColumnConfiguration.php
+++ src/Views/Traits/Configuration/LinkColumnConfiguration.php
@@ -5,24 +5,31 @@ namespace Rappasoft\LaravelLivewireTables\Views\Traits\Configuration;
 trait LinkColumnConfiguration
 {
     // TODO: Test
+    public function id(callable $callback): self
+    {
+        $this->idCallback = $callback;
+
+        return $this;
+    }
+
     public function title(callable $callback): self
     {
         $this->titleCallback = $callback;
-        
+
         return $this;
     }
 
     public function location(callable $callback): self
     {
         $this->locationCallback = $callback;
-        
+
         return $this;
     }
 
     public function attributes(callable $callback): self
     {
         $this->attributesCallback = $callback;
-        
+
         return $this;
     }
 }
