7ec50701462000a55926d4dc5dff832bee4621eb
Eduard Valdez <67836621+itsmeduard@users.noreply.github.com>
2022-04-17 10:17:23 +0800

    Update link.blade.php
    I modified this based on my needs for modal

--- resources/views/includes/columns/link.blade.php
+++ resources/views/includes/columns/link.blade.php
@@ -1,1 +1,8 @@
-<a href="{{ $path }}" {!! count($attributes) ? $column->arrayToAttributes($attributes) : '' !!}>{{ $title }}</a>
+{{--<a href="{{ $path }}" {!! count($attributes) ? $column->arrayToAttributes($attributes) : '' !!} --}}{{--wire:click="show({{ $row->id }})"--}}{{-->{{ $title }}</a>--}} {{--This code is the old one that I modified based on my needs--}}
+<a class='btn btn-{{ $title }} btn-icon-split' role='button' href='{{ $path }}' {!! count($attributes) ? $column->arrayToAttributes($attributes) : '' !!} data-bs-toggle='modal' @if($title == 'success') wire:click="edit({{ $id }})" @else wire:click="danger({{ $id }})" style='background: rgba(255,255,255,0);border-radius: 0px;border-width: 0px;border-style: none;'>
+    @if($title == 'success')
+        <i class='fas fa-pencil-alt' style='color: rgb(58,196,125);'></i>
+    @else
+        <i class='fas fa-trash-alt' style='color: #d92550;'></i>
+    @endif
+</a>
